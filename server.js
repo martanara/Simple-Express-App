2e9e8d9b9749f396f6be3e871b8945ede5dca39b
Marta Nara <marta.kos1230@gmail.com>
2022-03-24 18:10:11 +0100

    Add image handling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer')
 const path = require('path');
 const app = express();
 const hbs = require('express-handlebars');
@@ -7,6 +8,18 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
+
+var upload = multer({ storage: storage })
 
 app.use('/user', (req, res, next) => {
  res.render('forbidden.html');
@@ -30,6 +43,17 @@ app.get('/contact', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('project'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
